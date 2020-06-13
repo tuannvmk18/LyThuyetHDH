bb3a6e3956badf82641e00deb248111e61609614
Tuan <tuan01102000@gmail.com>
2020-06-13 19:07:36 +0700

    SJF

--- index.js
+++ index.js
@@ -99,6 +99,80 @@ var Scheduler = /** @class */ (function () {
         console.log(ahihi);
         return ahihi;
     };
+    Scheduler.prototype.SJF = function (Processes) {
+        var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o;
+        var ahihi = [];
+        for (var i = 0; i < Processes.length; i++) {
+            for (var j = 0; j < Processes[i].Tasks.length + 1; j++) {
+                ahihi[i + j] = [];
+            }
+        }
+        this.initialize(Processes);
+        var time = 0;
+        var count = 0;
+        do {
+            while (((_a = this.newQueue[0]) === null || _a === void 0 ? void 0 : _a.ArriveTime) == time) {
+                this.readyQueue.push(this.newQueue.shift());
+            }
+            while (((_b = this.waitingQueue[0]) === null || _b === void 0 ? void 0 : _b.ArriveTime) == time) {
+                this.readyQueue.push(this.waitingQueue.shift());
+            }
+            if (((_c = this.currentProcess) === null || _c === void 0 ? void 0 : _c.Tasks[0].Type) == 'CPU') {
+                this.readyQueue.sort(function (a, b) {
+                    return a.Tasks[0].Duration - b.Tasks[0].Duration;
+                });
+            }
+            if (this.currentProcess == undefined) {
+                if (this.readyQueue.length != 0) {
+                    this.currentProcess = this.readyQueue.shift();
+                    (_d = ahihi[count]) === null || _d === void 0 ? void 0 : _d.push(this.currentProcess.ID);
+                    (_e = ahihi[count]) === null || _e === void 0 ? void 0 : _e.push(time);
+                }
+            }
+            else {
+                if (((_g = (_f = this.currentProcess) === null || _f === void 0 ? void 0 : _f.Tasks[0]) === null || _g === void 0 ? void 0 : _g.Duration) > 0) {
+                    this.currentProcess.Tasks[0].Duration--;
+                }
+                if (((_j = (_h = this.currentProcess) === null || _h === void 0 ? void 0 : _h.Tasks[0]) === null || _j === void 0 ? void 0 : _j.Duration) == 0) {
+                    this.currentProcess.Tasks.shift();
+                    (_k = ahihi[count]) === null || _k === void 0 ? void 0 : _k.push(time);
+                    count++;
+                }
+                if (this.currentProcess.Tasks.length == 0) {
+                    this.currentProcess = undefined;
+                    // this.currentProcess = this.readyQueue.shift();
+                    // ahihi[count]?.push(this.currentProcess?.ID);
+                    // ahihi[count]?.push(time);
+                    continue;
+                }
+            }
+            if (((_m = (_l = this.currentProcess) === null || _l === void 0 ? void 0 : _l.Tasks[0]) === null || _m === void 0 ? void 0 : _m.Type) == 'IO') {
+                this.IOTask.push({
+                    ID: this.currentProcess.ID,
+                    Duration: this.currentProcess.Tasks[0].Duration,
+                    Type: 'IO',
+                    Start: time,
+                    End: time + this.currentProcess.Tasks[0].Duration
+                });
+                this.currentProcess.ArriveTime = time + this.currentProcess.Tasks[0].Duration;
+                this.currentProcess.Tasks.shift();
+                this.waitingQueue.push(this.currentProcess);
+                this.waitingQueue.sort(function (a, b) { return a.ArriveTime - b.ArriveTime; });
+                this.currentProcess = undefined;
+                continue;
+            }
+            time++;
+        } while (this.newQueue.length != 0 || this.readyQueue.length != 0 || this.waitingQueue.length != 0 || ((_o = this.currentProcess) === null || _o === void 0 ? void 0 : _o.Tasks.length) > 0);
+        ahihi.sort(function (a, b) {
+            if (a[0] < b[0])
+                return -1;
+            else if (a[0] > b[0])
+                return 1;
+            return 0;
+        });
+        console.log(ahihi);
+        return ahihi;
+    };
     return Scheduler;
 }());
 var tasks = [{ Duration: 2, Type: 'CPU' }, { Duration: 10, Type: 'IO' }, { Duration: 7, Type: "CPU" }];
@@ -109,4 +183,4 @@ var p2 = new Process('P2', 0, tasks2);
 var p3 = new Process('P3', 0, tasks3);
 var s = new Scheduler();
 // console.log(s);
-s.FCFS([p, p2, p3]);
+s.SJF([p, p2, p3]);
--- index.ts
+++ index.ts
@@ -127,6 +127,91 @@ class Scheduler {
         console.log(ahihi);
         return ahihi;
     }
+
+    SJF(Processes: Process[]): any {
+
+        let ahihi: [][] | any = [];
+
+
+        for(let i = 0; i < Processes.length; i++) {
+            for(let j = 0; j < Processes[i].Tasks.length + 1; j++) {
+                ahihi[i + j] = [];
+            }
+        }
+
+
+        this.initialize(Processes);
+        let time: number = 0;
+        let count = 0;
+
+        do {
+            while (this.newQueue[0]?.ArriveTime == time) {
+                this.readyQueue.push(this.newQueue.shift());
+            }
+
+            while (this.waitingQueue[0]?.ArriveTime == time) {
+                this.readyQueue.push(this.waitingQueue.shift());
+            }
+
+            if(this.currentProcess?.Tasks[0].Type == 'CPU') {
+                this.readyQueue.sort((a, b) => {
+                    return a.Tasks[0].Duration - b.Tasks[0].Duration;
+                });
+            }
+
+            if (this.currentProcess == undefined) {
+                if (this.readyQueue.length != 0) {
+                    this.currentProcess = this.readyQueue.shift();
+                    ahihi[count]?.push(this.currentProcess.ID);
+                    ahihi[count]?.push(time);
+                }
+            } else {
+                if (this.currentProcess?.Tasks[0]?.Duration > 0) {
+                    this.currentProcess.Tasks[0].Duration--;
+                }
+                if (this.currentProcess?.Tasks[0]?.Duration == 0) {
+                    this.currentProcess.Tasks.shift();
+                    ahihi[count]?.push(time);
+                    count++;
+                }
+
+                if (this.currentProcess.Tasks.length == 0) {
+                    this.currentProcess = undefined;
+                    // this.currentProcess = this.readyQueue.shift();
+                    // ahihi[count]?.push(this.currentProcess?.ID);
+                    // ahihi[count]?.push(time);
+                    continue;
+                }
+            }
+
+            if (this.currentProcess?.Tasks[0]?.Type == 'IO') {
+                this.IOTask.push({
+                    ID: this.currentProcess.ID,
+                    Duration: this.currentProcess.Tasks[0].Duration,
+                    Type: 'IO',
+                    Start: time,
+                    End: time + this.currentProcess.Tasks[0].Duration
+                });
+                this.currentProcess.ArriveTime = time + this.currentProcess.Tasks[0].Duration;
+                this.currentProcess.Tasks.shift();
+                this.waitingQueue.push(this.currentProcess);
+                this.waitingQueue.sort((a, b) => a.ArriveTime - b.ArriveTime);
+                this.currentProcess = undefined;
+                continue;
+            }
+            time++;
+        } while (this.newQueue.length != 0 || this.readyQueue.length != 0 || this.waitingQueue.length != 0 || this.currentProcess?.Tasks.length > 0);
+        
+
+        ahihi.sort((a, b) => {
+            if(a[0] < b[0])
+                return -1;
+            else if(a[0] > b[0]) return 1;
+            return 0;
+        });
+        console.log(ahihi);
+        return ahihi;
+    }
 }
 
 
@@ -139,6 +224,6 @@ let p2 = new Process('P2', 0, tasks2);
 let p3 = new Process('P3', 0, tasks3);
 let s = new Scheduler();
 // console.log(s);
-s.FCFS([p, p2, p3]);
+s.SJF([p, p2, p3]);
 
 
