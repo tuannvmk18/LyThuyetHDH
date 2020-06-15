3e82d751d48c88f46361482033f487af4ce674ad
Tuan <tuan01102000@gmail.com>
2020-06-15 13:02:42 +0700

    fix bug

--- main.js
+++ main.js
@@ -297,4 +297,82 @@ class Scheduler {
         console.log(ahihi);
         return ahihi;
     }
+    RR(Processes, Quantum) {
+        var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q;
+        let ahihi = [];
+        let count = 0;
+        for (let i = 0; i < Processes.length + 100; i++) {
+            ahihi[i] = [];
+        }
+        this.initialize(Processes);
+        let time = 0;
+        do {
+            while (((_a = this.newQueue[0]) === null || _a === void 0 ? void 0 : _a.ArriveTime) == time) {
+                this.readyQueue.push(this.newQueue.shift());
+            }
+            while (((_b = this.waitingQueue[0]) === null || _b === void 0 ? void 0 : _b.ArriveTime) == time) {
+                this.readyQueue.push(this.waitingQueue.shift());
+            }
+            if (this.currentProcess == undefined) {
+                if (this.readyQueue.length != 0) {
+                    this.currentProcess = this.readyQueue.shift();
+                    (_c = ahihi[count]) === null || _c === void 0 ? void 0 : _c.push(this.currentProcess.ID);
+                    (_d = ahihi[count]) === null || _d === void 0 ? void 0 : _d.push(time);
+                }
+            }
+            else {
+                if (((_f = (_e = this.currentProcess) === null || _e === void 0 ? void 0 : _e.Tasks[0]) === null || _f === void 0 ? void 0 : _f.Duration) > 0) {
+                    this.currentProcess.Tasks[0].Duration--;
+                }
+                if (time % Quantum == 0) {
+                    this.readyQueue.push(this.currentProcess);
+                    this.currentProcess = this.readyQueue.shift();
+                    (_g = ahihi[count]) === null || _g === void 0 ? void 0 : _g.push(time);
+                    count++;
+                    (_h = ahihi[count]) === null || _h === void 0 ? void 0 : _h.push(this.currentProcess.ID);
+                    (_j = ahihi[count]) === null || _j === void 0 ? void 0 : _j.push(time);
+                }
+                if (((_l = (_k = this.currentProcess) === null || _k === void 0 ? void 0 : _k.Tasks[0]) === null || _l === void 0 ? void 0 : _l.Duration) == 0) {
+                    this.currentProcess.Tasks.shift();
+                    (_m = ahihi[count]) === null || _m === void 0 ? void 0 : _m.push(time);
+                    count++;
+                }
+                if (this.currentProcess.Tasks.length == 0) {
+                    this.currentProcess = undefined;
+                    continue;
+                }
+            }
+            if (((_p = (_o = this.currentProcess) === null || _o === void 0 ? void 0 : _o.Tasks[0]) === null || _p === void 0 ? void 0 : _p.Type) == 'IO') {
+                this.IOTask.push({
+                    ID: this.currentProcess.ID,
+                    Duration: this.currentProcess.Tasks[0].Duration,
+                    Type: 'IO',
+                    Start: time,
+                    End: time + this.currentProcess.Tasks[0].Duration
+                });
+                this.currentProcess.ArriveTime = time + this.currentProcess.Tasks[0].Duration;
+                this.currentProcess.Tasks.shift();
+                this.waitingQueue.push(this.currentProcess);
+                this.waitingQueue.sort((a, b) => a.ArriveTime - b.ArriveTime);
+                this.currentProcess = undefined;
+                continue;
+            }
+            time++;
+        } while (this.newQueue.length != 0 || this.readyQueue.length != 0 || this.waitingQueue.length != 0 || ((_q = this.currentProcess) === null || _q === void 0 ? void 0 : _q.Tasks.length) > 0);
+        ahihi.sort((a, b) => {
+            if (a[0] < b[0])
+                return -1;
+            else if (a[0] > b[0])
+                return 1;
+            return 0;
+        });
+        ahihi = ahihi.filter(x => x.length != 0 && x[1] - x[2]);
+        ahihi.forEach((x) => {
+            x[1] *= 1000;
+            x[2] *= 1000;
+        });
+        console.log(ahihi);
+        draw(ahihi);
+        return ahihi;
+    }
 }
